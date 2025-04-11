8b7be9c9ea522ff3b49254b2d7168846928e5976
Shane K. Panter <shane@foundationcode.com>
2025-04-11 17:23:52 +0000

    changes

--- Hello.java
+++ Hello.java
@@ -1,14 +1,25 @@
 public class Hello {
 
 
-    public void greet() {
-        System.out.println("Hello, World from an object!");
+    public void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public String getNameFromUser() {
+        System.out.print("Please enter your name: ");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String name = scanner.nextLine();
+        scanner.close();
+        return name;
     }
 
     public static void main(String[] args) {
         Hello hello = new Hello();
-        hello.greet();
+        String name = hello.getNameFromUser();
+        hello.greet(name);
     }
+
+
 
 
 }
